284a44973fcf23b91ae5a5e5f944da9d0daba0d0
mdabusayedislam <abusayediit@gmail.com>
2022-01-02 00:01:43 +0600

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
-<h1 v-once>{{count}}</h1>
-<!-- <h1 v-if="isShowing" v-html="title"></h1>
-<h2 v-else-if="isShowing===null">Else if Block</h2>
-<h2 v-else>Else Block</h2> -->
-<p1>Something new</p1>
+<h1>DC Heroes</h1>
+ <div v-for="(product, num ) in dcHeroes" v-bind:key="num">
+   {{num}}:{{product.a}}
+ </div>
 </template>
 <script>
-export default{
+export default{  
     data(){
         return{
-            title:`<h1>Hey vue 3</h1>`,
-            isShowing:true,
-            count:0
+           dcHeroes:[{a:'a'},{a:'b'},{a:'c'},{a:'d'}]
         }
-    },
-    mounted(){
-            setInterval(()=>{
-this.count++;
-            },1000)
     }
 }
-
 </script>
