3936d652af6fb46d45a913ff411c63954e998ceb
mdabusayedislam <abusayediit@gmail.com>
2022-01-01 23:12:09 +0600

    directives

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-<h1>{{title}}</h1>
+<h1 v-once>{{count}}</h1>
+<!-- <h1 v-if="isShowing" v-html="title"></h1>
+<h2 v-else-if="isShowing===null">Else if Block</h2>
+<h2 v-else>Else Block</h2> -->
 <p1>Something new</p1>
 </template>
 <script>
 export default{
     data(){
         return{
-            title:`Hey vue 3`
+            title:`<h1>Hey vue 3</h1>`,
+            isShowing:true,
+            count:0
         }
+    },
+    mounted(){
+            setInterval(()=>{
+this.count++;
+            },1000)
     }
 }
 
