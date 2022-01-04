6cc728e57c83038e3ed8b3ea8b183723928f6ed1
mdabusayedislam <abusayediit@gmail.com>
2022-01-04 23:57:33 +0600

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
 <h1>DC Heroes</h1>
- <div v-for="(product, num ) in dcHeroes" v-bind:key="num">
+ <div v-for="(product, num ) in dcHeroes" v-bind:key="product">
    {{num}}:{{product.a}}
  </div>
+ <input v-model.lazy="newHero"/>
+ <button v-bind:disabled="isActive">Add</button>
 </template>
 <script>
 export default{  
     data(){
         return{
+           isActive:false,
+           newHero:"e",
            dcHeroes:[{a:'a'},{a:'b'},{a:'c'},{a:'d'}]
         }
     }
