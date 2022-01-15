400e035f9c4b62832fc3788f077d1da360e6f304
mdabusayedislam <abusayediit@gmail.com>
2022-01-15 16:49:10 +0600

    Add Component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,16 @@
 <template>
-      <h1>DC Heroes {{heroesCount}}</h1>
-      <ul>
-            <li v-for="(hero, index) in dcHeros" :key="index">
-                  <div>{{hero.name}}<button v-on:click="remove(index)">x</button></div>
-            </li>
-      </ul>     
-      <form @submit.prevent="addhero">
-            <input v-model="newHero" placeholder="Type Name"/>
-            <button type="submit">Add</button>
-      </form>
+   <AppHeader/>
+      <div class="w-full flex">
+            <div class="m-auto">
+                 <DcHeros/>            
+            </div>
+      </div>
 </template>
 <script>
-export default{ 
-    computed:{
-      heroesCount(){
-               return this.dcHeros.length;  
-      }    
-    },
-  methods:{
-            addhero(){
-                if(this.dcHero!==''){
-                      this.dcHeros.unshift({name:this.newHero});
-                      this.newHero="";
-                }               
-              },  
-            remove(index){
-                     this.dcHeros=this.dcHeros.filter((hero,i)=>i!=index)
-                },
-            },
-            data(){
-                    return{
-                      newHero:"",                                  
-                      dcHeros:[
-                            {name:'Superman'},
-                            {name:'Superwoman'},
-                            {name:'Batman'},
-                            {name:'Batwoman'}
-                            ]          
-                    }
-              },   
-           
+import AppHeader from './components/AppHeader';
+import DcHeros from './components/DcHeros';
+
+export default{   
+    components:{AppHeader,DcHeros}               
 }
 </script>
